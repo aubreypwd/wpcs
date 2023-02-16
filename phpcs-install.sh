c853f9b339d023d4cbe8746446be8825b29a4626
Aubrey Portwood <code@aubreypwd.com>
2023-02-16 08:30:41 -0700

    Delete installation.
    They should symlink or copy themselves.

--- phpcs-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-###
- # Symlinks configuration files for phpcs
- ##
-
-ln -sf ./vendor/aubreypwd/wpcs/configs/phpcs/.phpcs.xml.dist ./.phpcs.xml.dist
-ln -sf ./vendor/aubreypwd/wpcs/configs/phpcs/.editorconfig ./.editorconfig
